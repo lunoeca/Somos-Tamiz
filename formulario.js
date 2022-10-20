1ac12012422123313a4441159ea7e1c4d6e9f122
cotimaetta <92846050+cotimaetta@users.noreply.github.com>
2022-10-20 16:19:26 -0300

    Commit 3

--- formulario.js
+++ formulario.js
@@ -1,23 +1,27 @@
-var nombre = document.getElementById('nombre');
-var correo = document.getElementById('correo');
-var error = document.getElementById('error');
-error.style.color = 'red';
+function chequeo() {
 
+    var nombre = document.getElementById('nombre');
+    var correo = document.getElementById('correo');
+    var error = document.getElementById('error');
+    error.style.color = 'red';
 
-
-var form = document.getElementById('formulario');
-form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
     var mensajesError = [];
     if (nombre.value === null || nombre.value === '') {
         mensajesError.push('Ingresa tu nombre');
+        return false;
     }
     if (correo.value === null || correo.value === '') {
         mensajesError.push('Ingresa tu email');
+        return false;
     }
     if (!correo.value.includes('@')) {
         mensajesError.push('Corrige tu email');
+        return false;
     }
 
+
     error.innerHTML = mensajesError.join(', ');
-}); 
+
+    return true;
+}
+
