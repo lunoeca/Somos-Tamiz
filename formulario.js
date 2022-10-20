ca771ed51dd891aa4e4050f5637f7d604016f8f1
cotimaetta <92846050+cotimaetta@users.noreply.github.com>
2022-10-20 17:13:38 -0300

    form anda completamente

--- formulario.js
+++ formulario.js
@@ -3,25 +3,22 @@ function chequeo() {
     var nombre = document.getElementById('nombre');
     var correo = document.getElementById('correo');
     var error = document.getElementById('error');
-    error.style.color = 'red';
-
+    
     var mensajesError = [];
+    console.log('formulario'); 
     if (nombre.value === null || nombre.value === '') {
         mensajesError.push('Ingresa tu nombre');
-        return false;
     }
     if (correo.value === null || correo.value === '') {
         mensajesError.push('Ingresa tu email');
-        return false;
     }
     if (!correo.value.includes('@')) {
         mensajesError.push('Corrige tu email');
-        return false;
     }
 
 
     error.innerHTML = mensajesError.join(', ');
 
-    return true;
+    return false;
 }
 
